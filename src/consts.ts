a09c684e37b5651b9d91be4104c7b167b516d29f
Joseph Alberto Morthimer León Lau <joseph@leonlau.pe>
2024-05-02 16:45:19 -0500

    Site title and description.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Desarrollador mobile especializado en Flutter. Actualmente trabajo en Bitacora.io. Soy un comprometido con resolución de problemas; implementando y brindando mantenimiento a sistemas de acuerdo a las necesidades de cada proyecto al que me enfrento.';
+export const SITE_TITLE = 'Joseph León';
